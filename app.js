63b4a0483b15f98dc36b592fe491282394dd7015
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-13 16:21:01 +0100

    get and print weather details from weatherstack api
    weatherstack.com
    api.weatherstack.com

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const request =  require('postman-request')
+const request =  require('postman-request');
+const url = 'http://api.weatherstack.com/current?access_key=5c68b7ea2ba8518addfb9fbac098ac94&query=53.2953821,2.1402895&units=f';
+
+
+request(url, function(error, response, body){
+    const data = JSON.parse(body);
+    // console.log('ERROR', error);
+    // console.log('StatusCode', response && response.statusCode);
+    // console.log('Current weather: ', data.current)
+
+    console.log(data.current.weather_descriptions[0] + ' Its currently ' + data.current.temperature + ' degrees out but it feels like ' + data.current.feelslike + ' degrees');
+})
