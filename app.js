7a3e57ad71935105c8818eb8b22f51620648ea26
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-15 11:51:27 +0100

    Handling errors before getting the data from the api's

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ const url = 'http://api.weatherstack.com/current?access_key=5c68b7ea2ba8518addfb
 
 request(url, function (error, response, body) {
     const data = JSON.parse(body);
-    // console.log('ERROR', error);
-    // console.log('StatusCode', response && response.statusCode);
-    // console.log('Current weather: ', data.current)
-
-    console.log(data.current.weather_descriptions[0] + ' Its currently ' + data.current.temperature + ' degrees out but it feels like ' + data.current.feelslike + ' degrees');
+    if (error) {
+        console.log('unable to connect to weather services!');
+    } else if (data.error) {
+        console.log('unable to find locations');
+    } else {
+        console.log(data.current.weather_descriptions[0] + ' Its currently ' + data.current.temperature + ' degrees out but it feels like ' + data.current.feelslike + ' degrees');
+    }
 })
 
-const geolocURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJoaW5hdi1ndW5pc2hldHR5IiwiYSI6ImNrdXBvYWtqMzBmanIycXA1bHk5YW5mcWgifQ.EMZFAMDRxbX6JvZBQxPziw&limit=1';
+const geolocURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Newyork.json?access_token=pk.eyJ1IjoiYWJoaW5hdi1ndW5pc2hldHR5IiwiYSI6ImNrdXBvYWtqMzBmanIycXA1bHk5YW5mcWgifQ.EMZFAMDRxbX6JvZBQxPziw&limit=1';
 
 request(geolocURL, function (error, response, body) {
-    const data = JSON.parse(body);
-    console.log('Latitude: ' + data.features[0].center[0])
-    console.log('Longitude: ' + data.features[0].center[1])
+    if (error) {
+        console.log('unable to connect to map services');
+    } else if (JSON.parse(body).features.length <= 0) {
+        console.log('cannot find given location');
+    }
+    else {
+        const data = JSON.parse(body);
+        console.log('Latitude: ' + data.features[0].center[0])
+        console.log('Longitude: ' + data.features[0].center[1])
+    }
 })
