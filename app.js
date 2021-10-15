0942930cf4c68693cb434436b5c170cc9dd31d3a
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-15 14:08:48 +0100

    get location from command line and pass it to geocode callback

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 const chalk = require('chalk')
 
-geocode('Manchester', (error, data) => {
-    if (error) {
-        return error;
-    }
-    forecast(data.lat, data.long, (error, forecastData) => {
+
+const location = process.argv[2]
+
+
+if (!location) {
+    console.log('enter a location');
+} else {
+    geocode(location, (error, data) => {
         if (error) {
             return error;
         }
-        console.log('Error', error)
-        console.log('location: ', data.name)
-        console.log(chalk.greenBright(forecastData))
+        forecast(data.lat, data.long, (error, forecastData) => {
+            if (error) {
+                return error;
+            }
+            console.log('Error', error)
+            console.log('location: ', data.name)
+            console.log(chalk.greenBright(forecastData))
+        })
     })
-})
+
+
+}
