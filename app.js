c7115c5059a5c14d492ad1e8bfaea4e2c75fe06c
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-15 13:44:37 +0100

    created function for forecast and export it

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const request = require('postman-request');
 const geocode = require('./utils/geocode');
-// const url = 'http://api.weatherstack.com/current?access_key=5c68b7ea2ba8518addfb9fbac098ac94&query=53.2953821,2.1402895&units=f';
+const forecast = require('./utils/forecast')
 
 
-// request(url, function (error, response, body) {
-//     const data = JSON.parse(body);
-//     if (error) {
-//         console.log('unable to connect to weather services!');
-//     } else if (data.error) {
-//         console.log('unable to find locations');
-//     } else {
-//         console.log(data.current.weather_descriptions[0] + ' Its currently ' + data.current.temperature + ' degrees out but it feels like ' + data.current.feelslike + ' degrees');
-//     }
-// })
-
+forecast(44.1545, 2.1402895, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
 geocode('Manchester', (error, data) => {
     console.log('error: ', error);
