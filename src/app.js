62d278da2d583971c4c09afb797eebd547697acf
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-19 00:30:48 +0100

    pass address from querystring to geocode function and send forecast data as response

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
@@ -54,10 +56,25 @@ app.get('/weather', (req, res) => {
             error: 'Please enter a location'
         })
     }
-    res.send({
-        forecast: 'Its snowing',
-        address: req.query.address
-    });
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address,
+                test: 'testing'
+            });
+        })
+    })
 
 })
 
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5c68b7ea2ba8518addfb9fbac098ac94&query=' + latitude + ',' + longitude;
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ', Its currently ' + body.current.temperature + ' degrees out but it feels like ' + body.current.feelslike + ' degrees')
+        }
+    })
+}
+
+module.exports = forecast
