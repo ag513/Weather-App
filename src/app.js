cef8a2eaad6c863b4cfbc939b0371557684ea58a
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-18 23:42:53 +0100

    Added project template from nodeJS basics repo

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+
+const app = express();
+
+// define paths for express confid
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//setup directory for static assets
+app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'weather page',
+        name: 'abhi'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'about page',
+        name: 'abhinav'
+    });
+})
+
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'this is a help page',
+        name: 'abhinav'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'Cannot found help',
+        name: 'abhi'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please enter a location'
+        })
+    }
+    res.send({
+        forecast: 'Its snowing',
+        address: req.query.address
+    });
+
+})
+
+app.get('*', (req, res) => {
+    // res.send('cannot find the page')
+    res.render('404', {
+        errorMessage: 'This is 404  page',
+        name: 'abhi'
+    })
+})
+
+
+app.listen(3001, () => {
+    console.log('server running on port 3001');
+})
