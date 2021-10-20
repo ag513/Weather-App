cdd167dc0f1ec219b3d3896cee936366b5a52ee0
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-20 10:39:32 +0100

    Show fetched content on page

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,26 @@
-console.log('from scripts');
+const weatherFrom = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+const weatherIcon = document.querySelector('#weather-icon')
 
-fetch('http://localhost:3001/weather?address=!').then((res) => {
-    res.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-});
+weatherFrom.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value
+    message1.textContent = 'loading...'
+    message2.textContent = ''
+    weatherIcon.style.display = 'none'
+
+    fetch('http://localhost:3001/weather?address=' + location).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+            } else {
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+                weatherIcon.setAttribute('src', data.forecastIcon)
+                weatherIcon.style.display = 'block'
+            }
+        })
+    });
+})
