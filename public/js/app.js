2491c1d13da6a77bde47b0727d04816559d4044e
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-20 09:56:08 +0100

    Get weather data using fetch method

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-console.log('from scripts')
+console.log('from scripts');
+
+fetch('http://localhost:3001/weather?address=!').then((res) => {
+    res.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+});
